f0a8d55fbd646f16c8ccafc2417f93bff0b1c6d8
Josiah Brough <josiah.brough@fisherpaykel.com>
2023-04-04 22:59:02 +1200

    Small changes with Node methods

--- minimax_from_wiki.py
+++ minimax_from_wiki.py
@@ -1,17 +1,15 @@
 from math import inf
-from .games import Game
+from games import TicTacToe, Game
 
 class Node:
-    def __init__(self) -> None:
-        self.game: Game
-        self.children: list
+    def __init__(self, game) -> None:
+        self.game: Game = game
 
     def get_children(self) -> list: # The set of next possible moves
         return self.children
     
     def evaluate(self) -> int: # Get the numerical value for the current state
-        self.game.evaluate
-        pass
+        self.game.evaluate()
     
     def is_terminal(self) -> bool: # Player won or no more moves to play
         pass
@@ -32,4 +30,4 @@ def minimax(node: Node, depth: int, maximizing_player: bool):
     
 if __name__ == "__main__":
     origin = Node()
-    minimax(origin, inf, True)
+    minimax(TicTacToe(3), inf, True)
